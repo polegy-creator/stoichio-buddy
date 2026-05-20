3f91720d3210216bd1f484f474d05b6ebc5ed81b
Yonatan Poleg <yonatanpoleg@Yonatans-MacBook-Air.local>
2026-05-20 23:03:24 +0300

    Add literature density regression checks

--- test_density_engine.py
+++ test_density_engine.py
@@ -13,6 +13,61 @@ from density_engine import (
 from formula_parser import molar_mass, parse_formula
 
 
+LITERATURE_DENSITY_CASES = [
+    {
+        "name": "hematite Fe2O3, Blake et al. 1966, COD 9000139",
+        "formula": "Fe2O3",
+        "system": "Hexagonal",
+        "a": 5.038,
+        "c": 13.772,
+        "z": 6,
+        "published_volume": 302.722,
+        "published_density": 5.256,
+    },
+    {
+        "name": "rutile TiO2, Howard et al. 1991, COD 9015662",
+        "formula": "TiO2",
+        "system": "Tetragonal",
+        "a": 4.5937,
+        "c": 2.9587,
+        "z": 2,
+        "published_volume": 62.435,
+        "published_density": 4.249,
+    },
+    {
+        "name": "zincite ZnO, Schreyer et al. 2014, COD 2300450",
+        "formula": "ZnO",
+        "system": "Hexagonal",
+        "a": 3.249308,
+        "c": 5.205709,
+        "z": 2,
+        "published_volume": 47.5984,
+        "published_density": 5.67848,
+    },
+    {
+        "name": "baddeleyite ZrO2, Smith and Newkirk 1965, COD 9007485",
+        "formula": "ZrO2",
+        "system": "Monoclinic",
+        "a": 5.145,
+        "b": 5.2075,
+        "c": 5.3107,
+        "beta": 99.23,
+        "z": 4,
+        "published_volume": 140.445,
+        "published_density": 5.828,
+    },
+    {
+        "name": "spinel Co3O4, Kotousova and Polyakov 1972, COD 1526734",
+        "formula": "Co3O4",
+        "system": "Cubic",
+        "a": 8.065,
+        "z": 8,
+        "published_volume": 524.582,
+        "published_density": None,
+    },
+]
+
+
 class DensityEngineTests(unittest.TestCase):
     def test_unit_cell_volume_lattice_systems(self):
         self.assertAlmostEqual(unit_cell_volume_from_lattice("Cubic", 2.0), 8.0)
@@ -22,6 +77,22 @@ class DensityEngineTests(unittest.TestCase):
         density = theoretical_density_from_cell("Fe2O3", 302.0, 6)
         expected = 6 * molar_mass(parse_formula("Fe2O3")) / AVOGADRO / (302.0 * ANGSTROM3_TO_CM3)
         self.assertAlmostEqual(density, expected, places=10)
+
+    def test_theoretical_density_matches_literature_cells(self):
+        for case in LITERATURE_DENSITY_CASES:
+            with self.subTest(case=case["name"]):
+                volume = unit_cell_volume_from_lattice(
+                    case["system"],
+                    case["a"],
+                    b_a=case.get("b"),
+                    c_a=case.get("c"),
+                    beta_deg=case.get("beta"),
+                )
+                density = theoretical_density_from_cell(case["formula"], volume, case["z"])
+
+                self.assertAlmostEqual(volume, case["published_volume"], delta=0.001)
+                if case["published_density"] is not None:
+                    self.assertAlmostEqual(density, case["published_density"], delta=0.001)
 
     def test_pellet_mass_and_relative_density(self):
         volume = cylinder_volume_cm3(25.05, 1.0)
